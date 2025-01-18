e30d5dcd65c82c439ce24c04e76a41bcd60b0502
hodaifa <150153831+hodaifa-ech@users.noreply.github.com>
2025-01-18 15:54:45 +0100

    add private ip for kafka

--- src/main/java/com/fintech/walletservice/config/KafkaProducerConfig.java
+++ src/main/java/com/fintech/walletservice/config/KafkaProducerConfig.java
@@ -20,7 +20,7 @@ public class KafkaProducerConfig {
   @Bean
   public ProducerFactory<String, TransactionRequest> producerFactory() {
     Map<String, Object> configProps = new HashMap<>();
-    configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+    configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "10.0.0.26:9092");
     configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
     configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
 //    configProps.put(JsonSerializer.TYPE_MAPPINGS,
